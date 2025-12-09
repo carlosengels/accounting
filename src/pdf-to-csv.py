2f15aaf1bd670c3e34d77383751281b7ef2f4d87
CJE <engels.carlos@gmail.com>
2025-12-09 14:54:39 -0700

    Change rest of script into functions

--- src/pdf-to-csv.py
+++ src/pdf-to-csv.py
@@ -4,29 +4,32 @@ from io import StringIO
 import re
 import logging
 
-#Read text from PDF
-reader = PdfReader("../artifacts/Statements.pdf")
-page = reader.pages[2]
-page_content = page.extract_text()
-match = re.search(
-    r"ACCOUNT ACTIVITY.*?\$ Amount\s*(.*?)\s*Total fees charged in",
-    page_content,
-    flags=re.DOTALL
-)
-if match:
-    account_activity = match.group(1).strip()
-else:
-    account_activity = None
-
-account_activity_lines_list = account_activity.split("\n")
+#Variables
 account_activity_header_csv = "Date of Transaction,Description,$ Amount,Category,Sub-Category\n"
+reader = PdfReader("../artifacts/Statements.pdf")
 
-#Get date from PDF
-page = reader.pages[0]
-page_content = page.extract_text()
-pattern = r"(\d{1,2}\/\d{1,2}\/\d{2})\s*-\s*(\d{1,2}\/\d{1,2}\/\d{2})"
-match = re.search(pattern, page_content)
-statement_end_date = match.groups()[1].replace("/","-")
+
+def get_account_activity_str_from_pdf () :
+    page = reader.pages[2]
+    page_content = page.extract_text()
+    match = re.search(
+        r"ACCOUNT ACTIVITY.*?\$ Amount\s*(.*?)\s*Total fees charged in",
+        page_content,
+        flags=re.DOTALL
+    )
+    if match:
+        account_activity = match.group(1).strip()
+    else:
+        account_activity = None
+
+    return account_activity.split("\n")
+
+def get_date_from_pdf () :
+    page = reader.pages[0]
+    page_content = page.extract_text()
+    pattern = r"(\d{1,2}\/\d{1,2}\/\d{2})\s*-\s*(\d{1,2}\/\d{1,2}\/\d{2})"
+    match = re.search(pattern, page_content)
+    return match.groups()[1].replace("/","-")
 
 def redact (transaction) :
     if "SIMMS" in transaction:
@@ -46,7 +49,6 @@ def categorize (transaction) :
         return "Pets"
     return ""
 
-#Convert lines to CSV format
 def convert_account_activity_content_to_csv (list_of_activity_lines) :
     converted_activity_line_str = ""
     for line in list_of_activity_lines:
@@ -60,7 +62,6 @@ def convert_account_activity_content_to_csv (list_of_activity_lines) :
             converted_activity_line_str += statement
     return converted_activity_line_str
 
-#Write to csv
 def write_to_csv (pdf_content_str, statement_end_date_str) :
     filename = "../artifacts/monthly_statement_{date_str}.csv".format(date_str=statement_end_date_str)
     df = pd.read_csv(StringIO(pdf_content_str))
@@ -69,9 +70,9 @@ def write_to_csv (pdf_content_str, statement_end_date_str) :
 
 def main():
     logging.info("Starting main function")
-    account_activity_content_csv = convert_account_activity_content_to_csv(account_activity_lines_list)
+    account_activity_content_csv = convert_account_activity_content_to_csv(get_account_activity_str_from_pdf())
     account_activity_complete_csv = account_activity_header_csv + account_activity_content_csv
-    write_to_csv(account_activity_complete_csv, statement_end_date)
+    write_to_csv(account_activity_complete_csv, get_date_from_pdf())
 
 if __name__ == "__main__":
     logging.basicConfig(
